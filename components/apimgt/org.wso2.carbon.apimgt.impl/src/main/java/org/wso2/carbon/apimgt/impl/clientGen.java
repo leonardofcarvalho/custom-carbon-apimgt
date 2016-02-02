999030f1c10491aad6deb82aaa7aa14376ce0abe
rnavagamuwa <randikan@wso2.com>
2016-02-02 17:30:03 +0530

    clientGen class updated

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/clientGen.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/clientGen.java
@@ -1,20 +1,30 @@
 package org.wso2.carbon.apimgt.impl;
 
-import org.apache.openjpa.lib.log.Log;
-import org.wso2.carbon.apimgt.impl.utils.APIUtil;
-
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
+import org.wso2.carbon.apimgt.api.model.Subscriber;
 
 /**
  * Created by randika on 2/1/16.
  */
 public class clientGen {
 
-    public static void sdkGeneration(String appId, String sdkLanguage){
+    public static void sdkGeneration(String appName, String sdkLanguage,String userName, String groupId){
         System.out.println(sdkLanguage);
-        Log.INFO(sdkLanguage);
+
+        Subscriber currentSubscriber = null;
+        try {
+            currentSubscriber = ApiMgtDAO.getSubscriber(userName);
+            ApiMgtDAO DAO = new ApiMgtDAO();
+            DAO.getSubscribedAPIs(currentSubscriber,appName,groupId);
+        } catch (APIManagementException e) {
+            e.printStackTrace();
+        }
+
+
+        }
+
+
         //APIUtil.getSwagger20DefinitionFilePath();
-    }
+
 }
